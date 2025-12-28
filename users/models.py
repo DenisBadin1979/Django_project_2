1690492e4ab8c1abc26f436e25316f34533f646f
Denis_Badin <denisbadin@yandex.ru>
2025-12-29 03:39:42 +0500

    branch 30.2 версия 1.0

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from lms.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -28,3 +31,64 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CASH = "cash"
+    PAYMENT_METHOD_TRANSFER = "transfer"
+
+    PAYMENT_METHOD_CHOICES = [
+        (PAYMENT_METHOD_CASH, "Наличные"),
+        (PAYMENT_METHOD_TRANSFER, "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+        help_text="Выберите пользователя",
+        related_name="payments",
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата платежа",
+        help_text="Введите дауту платежа",
+    )
+    paid_course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="payments",
+        verbose_name="Оплаченный курс",
+        help_text="Введите Оплаченный курс",
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="payments",
+        verbose_name="Оплаченный урок",
+        help_text="Введите Оплаченный урок",
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Сумма оплаты",
+        help_text="Введите сумму оплаты",
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name="Способ оплаты",
+        help_text="Выберите способ оплаты",
+    )
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+        ordering = ["-payment_date"]
